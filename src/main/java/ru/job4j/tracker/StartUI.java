9e3333a565b104e4aa40f378d8c1b0a8bd892c19
rsitdikov <rsitdikov.usp@gmail.com>
2021-06-07 09:46:45 +0500

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item first = new Item("first item");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String created = first.getCreated().format(formatter);
+        System.out.println(created);
+    }
+}
+
