49cf1f4832ca4f7527a2fffebc295268c8cd1b7a
rsitdikov <rsitdikov.usp@gmail.com>
2021-06-10 17:43:01 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #29929]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        for (int i = 10; i > -2; i--) {
+            System.out.println("numver: " + i + " factorial: " + new Fact().calc(i));
+        }
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Invalid argument");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
