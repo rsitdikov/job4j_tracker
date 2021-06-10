bf16a7e8d6a8fe1621d244f0ead4fa1b04854f65
rsitdikov <rsitdikov.usp@gmail.com>
2021-06-10 18:06:33 +0500

    0.4. Пользовательские исключения. [#219367 #29927]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rsl = index;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "banana", "orange", "mango"};
+        String required = "pear";
+        try {
+            System.out.println("A " + required + " in the list of fruits has an index: "
+                                + indexOf(fruits, required));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
