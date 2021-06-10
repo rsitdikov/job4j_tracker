02c8038d2582897b9de0689871f4b42ef88e4a3c
rsitdikov <rsitdikov.usp@gmail.com>
2021-06-10 22:51:10 +0500

    1. Организовать сортировку User [#10034 #29899]

---
+++ src/test/java/ru/job4j/collection/UserTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class UserTest {
+    @Test
+    public void whenAsc() {
+        Set<User> users = new TreeSet<>(
+                Set.of(
+                        new User("Petr", 32),
+                        new User("Ivan", 31)
+                )
+        );
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Petr", 32)));
+    }
+
+    @Test
+    public void whenComparePetrVSIvan() {
+        int rsl = new User("Petr", 32)
+                .compareTo(
+                        new User("Ivan", 31)
+                );
+        assertThat(rsl, greaterThan(0));
+    }
+}
