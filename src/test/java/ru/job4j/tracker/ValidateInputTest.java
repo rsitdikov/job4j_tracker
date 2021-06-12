ca3b60ebc78642d989a55704a95937e04444a737
rsitdikov <rsitdikov.usp@gmail.com>
2021-06-12 09:52:10 +0500

    2.1. Тест на ValidateInput [#311887 #166617] - исправления 2

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -33,12 +33,18 @@ public class ValidateInputTest {
     public void whenMultiValidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"});
+                new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "1000"});
         ValidateInput input = new ValidateInput(out, in);
-        for (int expected = 0; expected <= 9; expected++) {
-            int selected = input.askInt("Enter menu:");
-            assertThat(selected, is(expected));
-        }
+        assertThat(input.askInt("Enter menu:"), is(0));
+        assertThat(input.askInt("Enter menu:"), is(1));
+        assertThat(input.askInt("Enter menu:"), is(2));
+        assertThat(input.askInt("Enter menu:"), is(3));
+        assertThat(input.askInt("Enter menu:"), is(4));
+        assertThat(input.askInt("Enter menu:"), is(5));
+        assertThat(input.askInt("Enter menu:"), is(6));
+        assertThat(input.askInt("Enter menu:"), is(7));
+        assertThat(input.askInt("Enter menu:"), is(8));
+        assertThat(input.askInt("Enter menu:"), is(1000));
     }
 
     @Test
