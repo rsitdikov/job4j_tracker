2636df3fc30fa03c84bd56ba2f4fc03a18366d0d
rsitdikov <rsitdikov.usp@gmail.com>
2021-07-11 21:34:28 +0500

    2. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private static Tracker tracker = null;
+
+    private SingleTracker() {
+
+    }
+
+    public static Tracker getTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+}
