fea5427f921359a169ef603a478829dd0ebb3181
rsitdikov <rsitdikov.usp@gmail.com>
2021-08-17 13:46:07 +0500

    2. Отсортировать департаменты [#34352 #29903]

---
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DepartmentsTest {
+    @Test
+    public void whenMissed() {
+        List<String> input = Arrays.asList("k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChange() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenAscSort() {
+        List<String> input = Arrays.asList("k2/sk1", "k1/sk2", "k1", "k2");
+        List<String> expect = Arrays.asList("k1", "k1/sk2", "k2", "k2/sk1");
+        Departments.sortAsc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void whenDescSort() {
+        List<String> input = Arrays.asList("k2/sk1", "k1/sk2", "k1", "k2");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k1", "k1/sk2");
+        Departments.sortDesc(input);
+        assertThat(input, is(expect));
+    }
+}
+
