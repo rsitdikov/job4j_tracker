5992346ab7eccb5791c3ff558671d3bc03bce223
rsitdikov <rsitdikov.usp@gmail.com>
2021-08-20 13:53:26 +0500

    8. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,33 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit = " + "\""
+                + suit + "\""
+                + ", value = " + value
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        List<Card> deck = Stream.of(Suit.values())
+                .flatMap(s -> Stream.of(Value.values())
+                        .map(v -> new Card(s, v)))
+                .collect(Collectors.toList());
+        deck.stream()
+                .forEach(System.out::println);
+    }
+}
